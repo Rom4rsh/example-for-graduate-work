2409a544570f6004a8460baeaf86d4e5f8690012
Iron Levyy <olegan.plav@gmail.com>
2025-08-09 15:44:02 +0300

    Сделал CommentsController

--- src/main/java/ru/skypro/homework/controller/CommentsController.java
+++ src/main/java/ru/skypro/homework/controller/CommentsController.java
@@ -1,22 +1,156 @@
 package ru.skypro.homework.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import ru.skypro.homework.dto.Comment;
+import ru.skypro.homework.dto.Comments;
+import ru.skypro.homework.dto.CreateOrUpdateComment;
+import ru.skypro.homework.service.CommentsService;
+
+import javax.validation.Valid;
 
 @Slf4j
 @CrossOrigin(value = "http://localhost:3000")
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/ads/{id}/comments")
 public class CommentsController {
 
-   // @Operation(summary = "Получение комментариев объявления")
-   // @GetMapping
-   // ResponseEntity<>
+    private final CommentsService commentsService;
+
+    @Operation(
+            summary = "Получить все комментарии объявления",
+            description = "Возвращает список всех комментариев для указанного объявления",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Комментарии найдены",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Comments.class))
+
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Объявление не найдено"
+                    )
+            }
+    )
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public Comments getComments(@Parameter(description = "ID объявления", required = true) @PathVariable Integer id) {
+        return commentsService.getCommentsByAdId(id);
+    }
+
+    @Operation(
+            summary = "Добавить комментарий",
+            description = "Добавляет новый комментарий к указанному объявлению",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Комментарий создан",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Comment.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Требуется аутентификация"
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Объявление не найдено"
+                    )
+            }
+    )
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Comment addComment(
+            @Parameter(description = "ID объявления", required = true) @PathVariable Integer id,
+            @RequestBody @Valid CreateOrUpdateComment commentDto,
+            Authentication authentication
+    ) {
+        return commentsService.addComment(id, commentDto, authentication);
+    }
+
+    @Operation(
+            summary = "Удалить комментарий",
+            description = "Удаляет комментарий по его ID из указанного объявления",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "Комментарий удален"
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Требуется аутентификация"
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Нет прав для удаления комментария"
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Объявление или комментарий не найдены"
+                    )
+            }
+    )
+    @DeleteMapping("/{commentId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteComment(
+            @Parameter(description = "ID объявления", required = true) @PathVariable Integer id,
+            @Parameter(description = "ID комментария", required = true) @PathVariable Integer commentId
+    ) {
+        commentsService.deleteComment(id, commentId);
+    }
+
+    @Operation(
+            summary = "Обновить комментарий",
+            description = "Обновляет текст указанного комментария",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Комментарий обновлен",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Comment.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Требуется аутентификация"
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Нет прав для обновления комментария"
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Объявление или комментарий не найдены"
+                    )
+            }
+    )
+    @PatchMapping("/{commentId}")
+    @ResponseStatus(HttpStatus.OK)
+    public Comment updateComment(
+            @Parameter(description = "ID объявления", required = true) @PathVariable Integer id,
+            @Parameter(description = "ID комментария", required = true) @PathVariable Integer commentId,
+            @RequestBody @Valid CreateOrUpdateComment updatedComment
+    ) {
+        return commentsService.updateComment(id, commentId, updatedComment);
+    }
 
 }
--- src/main/java/ru/skypro/homework/service/impl/CommentsServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/CommentsServiceImpl.java
@@ -1,6 +1,31 @@
 package ru.skypro.homework.service.impl;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import ru.skypro.homework.dto.Comment;
+import ru.skypro.homework.dto.Comments;
+import ru.skypro.homework.dto.CreateOrUpdateComment;
 import ru.skypro.homework.service.CommentsService;
 
+@Service
 public class CommentsServiceImpl implements CommentsService {
+    @Override
+    public Comments getCommentsByAdId(Integer adId) {
+        return null;
+    }
+
+    @Override
+    public Comment addComment(Integer adId, CreateOrUpdateComment comment, Authentication auth) {
+        return null;
+    }
+
+    @Override
+    public void deleteComment(Integer adId, Integer commentId) {
+
+    }
+
+    @Override
+    public Comment updateComment(Integer adId, Integer commentId, CreateOrUpdateComment comment) {
+        return null;
+    }
 }
